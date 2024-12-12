de686e9871e0fdb1c5f7188fc285469640505559
Nael Reinhard <nael.reinhard@swenex.ch>
2024-12-12 09:27:09 +0100

    formatter run after revert

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const myUnusedVariable = 42;
 
-if (
-  myUnusedVariable ===
-  42
-) {
-  console.log(
-    'This will trigger eqeqeq rule',
-  );
+if (myUnusedVariable === 42) {
+  console.log('This will trigger eqeqeq rule');
 }
